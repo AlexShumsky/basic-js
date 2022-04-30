bd0addc36c70f461c5dba34797910b403a135770
AlexShumsky <sanya.shumskiy.95@mail.ru>
2022-04-30 12:07:47 +0300

    feat: add dns-stats + dream-team

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const result = {};
+  const sortedDomains = domains.map((domain) => domain.split(".").reverse());
+  sortedDomains.forEach((domain) =>
+    domain.reduce((newDomain, el) => {
+      const customDomain = newDomain + "." + el;
+      if (result[customDomain]) {
+        result[customDomain] += 1;
+      } else {
+        result[customDomain] = 1;
+      }
+      return customDomain;
+    }, "")
+  );
+  return result;
 }
-
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
