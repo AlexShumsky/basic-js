83af7f6da51bf0131d526dfb026705ce8c10e40e
AlexShumsky <sanya.shumskiy.95@mail.ru>
2022-05-03 02:35:57 +0300

    feat add base solution to vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -30,11 +30,48 @@ class VigenereCipheringMachine {
   }
   encrypt(...args) {
     this.argumentsCheck(...args);
+    let [message, key] = args;
+    key = key.toUpperCase();
+    let encrypted = "";
+    for (let i = 0, j = 0; i < message.length; i++) {
+      let currentChar = message[i].toUpperCase();
+      if (currentChar.charCodeAt() >= 65 && currentChar.charCodeAt() <= 90) {
+        encrypted += String.fromCharCode(
+          ((key[j % key.length].charCodeAt() -
+            65 +
+            (currentChar.charCodeAt() - 65)) %
+            26) +
+            65
+        );
+        j++;
+      } else {
+        encrypted += currentChar;
+      }
+    }
+    return encrypted;
   }
   decrypt(...args) {
     this.argumentsCheck(...args);
+    let [message, key] = args;
+    key = key.toUpperCase();
+    let decrypted = "";
+    for (let i = 0, j = 0; i < message.length; i++) {
+      let currentChar = message[i].toUpperCase();
+      if (currentChar.charCodeAt() >= 65 && currentChar.charCodeAt() <= 90) {
+        decrypted += String.fromCharCode(
+          ((currentChar.charCodeAt() - key[j % key.length].charCodeAt() + 26) %
+            26) +
+            65
+        );
+        j++;
+      } else {
+        decrypted += currentChar;
+      }
+    }
+    return decrypted;
   }
 }
+const test = new VigenereCipheringMachine();
 
 module.exports = {
   VigenereCipheringMachine,
