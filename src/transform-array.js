4664d91e7c0fac16b9fcb4e42785f9515fdc9bd0
AlexShumsky <sanya.shumskiy.95@mail.ru>
2022-05-03 00:56:48 +0300

    feat: impruve transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,33 +13,33 @@ const { NotImplementedError } = require("../extensions/index.js");
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
+
 function transform(arr) {
   if (!(arr instanceof Array))
     throw new Error("'arr' parameter must be an instance of the Array!");
-  const direction = {
-    prev: -1,
-    next: 1,
-  };
-  let result = arr;
+
+  let result = Array.from(arr);
   for (let i = 0; i < arr.length; i++) {
-    if (typeof arr[i] === "string") {
-      const commands = arr[i].split("-");
-      if (commands[2] === "double") {
-        result.splice(result[i - 1], 1, result[i + direction[commands[3]]]);
-      } else if (commands[2] === "discard") {
-        //result.splice(i + direction[commands[3]] + direction[commands[3]] === 'next' ?  -1 : 0, 2));
-        result.splice(
-          i + (commands[3] === "prev" ? direction[commands[3]] : 0),
-          2
-        );
-      }
+    if (result[i] === "--double-prev") {
+      if (i === 0) result.splice(i, 1);
+      else result.splice(i, 1, result[i - 1]);
+    } else if (result[i] === "--double-next") {
+      if (i === arr.length - 1) result.splice(i, 1);
+      else result.splice(i, 1, result[i + 1]);
+    } else if (result[i] === "--discard-prev") {
+      if (i === 0) result.splice(i, 1);
+      else result.splice(i - 1, 2);
+    } else if (result[i] === "--discard-next") {
+      if (i === arr.length - 1) result.splice(i, 1);
+      else result.splice(i, 2);
     }
   }
   return result;
 }
-//console.log(transform([1, 2, 3, "--double-prev", 4, 5])); // => [1, 2, 3, 4, 4, 5]
-console.log(transform([1, 2, 3, "--discard-prev", 4, 5])); // => [1, 2, 3, 4, 4, 5]
-console.log(transform([1, 2, 3, "--discard-next", 4, 5])); // => [1, 2, 3, 4, 4, 5]
+
+console.log(transform(["--discard-prev", 1, 2, 3]));
+console.log(transform([1, 2, 3, "--double-prev", 4, 5]));
+
 module.exports = {
   transform,
 };
